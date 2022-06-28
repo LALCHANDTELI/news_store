1a46a32f20ff9603404002a4e9c8d9948f9018d9
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2022-06-28 17:17:38 +0530

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,3 @@
 export const API_KEY = "UIwcC2cSBCGMUTfgw6U2kTkSsxj3Zez6qSN-LrSIp3E";
 export const API_SEARCH_URL = "https://api.newscatcherapi.com/v2/search?";
-export const API_URL =
-  "https://api.newscatcherapi.com/v2/latest_headlines?countries=IN&lang=hi&page_size=50";
+export const API_URL = "https://api.newscatcherapi.com/v2/latest_headlines?countries=IN&lang=hi&page_size=50";
